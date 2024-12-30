ff3245ba19bc781d054d5b6990a76921e6f4bf3e
agultekin <attin83@gmail.com>
2024-12-30 16:25:31 +0100

    - Adding trigger extractor - Adding pg functions extractor - Adding pg types extractors

--- lib/fix_db_schema_conflicts/schema_dumper.rb
+++ lib/fix_db_schema_conflicts/schema_dumper.rb
@@ -1,8 +1,16 @@
 require 'delegate'
+require_relative 'postgres_schema_info_extractor'
+require 'fix_db_schema_conflicts/triggers_operations'
+require 'fix_db_schema_conflicts/postgres_details_extractor'
 
 module FixDBSchemaConflicts
   module SchemaDumper
     class ConnectionWithSorting < SimpleDelegator
+      def initialize(connection)
+        super(connection)
+        @schema_info_extractor = FixDBSchemaConflicts::PostgresSchemaInfoExtractor.new(__getobj__)
+      end
+
       def extensions
         __getobj__.extensions.sort
       end
@@ -16,12 +24,36 @@ module FixDBSchemaConflicts
       end
 
       def foreign_keys(table)
-        __getobj__.indexes(table).sort_by(&:name)
+        __getobj__.foreign_keys(table).sort_by(&:name)
+      end
+
+      def pg_functions
+        @schema_info_extractor.pg_functions
+      end
+
+      def fetch_enum_types
+        @schema_info_extractor.fetch_enum_types
       end
 
       def triggers(table)
-        puts "called triggers"
-        __getobj__.triggers(table).sort_by(&:name)
+        query = <<-SQL
+          SELECT tgname AS name, pg_get_triggerdef(oid) AS definition
+          FROM pg_trigger
+          WHERE tgrelid = '#{table}'::regclass AND NOT tgisinternal;
+        SQL
+
+        __getobj__.execute(query).map do |row|
+          sanitized_sql = sanitize_trigger_definition(row['definition'])
+          OpenStruct.new(name: row['name'], definition: sanitized_sql)
+        end
+      end
+
+      def sanitize_trigger_definition(definition)
+        definition.gsub(/ON\s+(\w+\.)?(\w+\.)?/, 'ON ')
+      end
+
+      def fts_configurations
+        @schema_info_extractor.fts_configurations
       end
     end
 
@@ -33,36 +65,44 @@ module FixDBSchemaConflicts
 
     def tables(stream)
       specific = ENV['SCHEMA'] || if defined? ActiveRecord::Tasks::DatabaseTasks
-        File.join(ActiveRecord::Tasks::DatabaseTasks.db_dir, 'specific.rb')
-      else
-        "#{Rails.root}/db/specific.rb"
-      end
+                                    File.join(ActiveRecord::Tasks::DatabaseTasks.db_dir, 'specific.rb')
+                                  else
+                                    "#{Rails.root}/db/specific.rb"
+                                  end
 
       if File.exist?(specific)
         stream.puts("\t" + File.read(specific).gsub("\n", "\n\t") + "\n\n")
       end
 
+      with_sorting do
+        @details_operations = FixDBSchemaConflicts::PostgresDetailsExtractor.new(@connection)
+        @details_operations.create_types(stream)
+        @details_operations.create_functions(stream)
+        @details_operations.create_fts_configurations(stream)
+      end
       super(stream)
     end
 
-    def table(*args)
+    def table(table_name, stream, *args)
       with_sorting do
-        super(*args)
-      end
-    end
+        super(table_name, stream, *args)
 
-    def triggers(*args)
-      puts "main triggers"
-      with_sorting do
-        super(*args)
+        if @triggers_operations.nil?
+          @triggers_operations = FixDBSchemaConflicts::TriggersOperations.new(@connection)
+          @triggers_operations.reset_triggers_folder
+        end
+        @triggers_operations.triggers_creation(table_name, stream)
       end
     end
 
     def with_sorting
-      old, @connection = @connection, ConnectionWithSorting.new(@connection)
-      result = yield
-      @connection = old
-      result
+      old_connection = @connection
+      @connection = ConnectionWithSorting.new(old_connection)
+      begin
+        yield
+      ensure
+        @connection = old_connection
+      end
     end
   end
 end
